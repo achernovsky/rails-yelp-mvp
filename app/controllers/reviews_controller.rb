ee1c368c698a80cab2b74be7178470295d9657ed
Alex Chernovsky <alexche8888@gmail.com>
2020-03-26 19:40:18 +0200

    added link to new review from show rest

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = @Restaurant
+    @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
   end
